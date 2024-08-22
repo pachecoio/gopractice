60b1a8a0fc0bfa59c782493ae0cd01fb3564ed14
SAMANTHA GADET <samantha@merinio.com>
2024-08-22 15:10:15 +0200

    Fix Fibonacci

--- exercices/exercice_three.go
+++ exercices/exercice_three.go
@@ -1,12 +1,13 @@
 package exercices
 
-func FibonacciNumber(n int) int {
+func Fibonacci(n int) int {
 	if n == 0 {
 		return 0
 	}
-	if n == 1 {
+	if n < 2 {
 		return 1
 	}
-	return (n - 1) + (n - 2)
+
+	return Fibonacci(n-1) + Fibonacci(n-2)
 
 }
